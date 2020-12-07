4769ee3e2e08d590ac2020e62b0262e721ebeb3a
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-07 23:55:13 +0100

    Update eljefe-bot1.js

--- eljefe-bot1.js
+++ eljefe-bot1.js
@@ -3,31 +3,21 @@
 	var Discord = require('discord.js');
 	var bot = new Discord.Client();
 	var fs = require('fs');
-
-	var userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
-	var profanities = JSON.parse(fs.readFileSync('Storage/profanities.json', 'utf8'));
-	var profanities2 = JSON.parse(fs.readFileSync('Storage/profanities2.json', 'utf8'));
+	var ytdl = require('ytdl-core');
 
 	var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
 
 	var prefix = process.env.prefix;
 	var owner = process.env.ownerID;
-	var swearword;
 
-	function userInfo(user){
-		var finalString = '';
-		finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
-
-		var userCreated = user.createdAt.toString().split(' ');
-		finalString += 'Date created **' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**\n';
-
-		finalString += 'Message sent to this server: **' + userData[user.id].messagesSent + '** messages';
-		return finalString;
-	}
+	var servers = {};
+	var server;
+	var joinstatus='waiting';
 
 	bot.on('message', function(message){
 		var sender = message.author;
-		var input = message.content.toUpperCase();
+		var args = message.content.toString().split(' ');
+		var input = args[0].toUpperCase();
 
 		if((sender.id === '781250071215472640') || (sender.id === '781277535232458763')){
 			return;
@@ -37,122 +27,111 @@
 			message.channel.send({embed:{
 				description:commandsList,
 				color:0x2471A3
-			}})
+			}});
 		}
 		if(input === prefix + 'COMMANDS'){
 			message.channel.send({embed:{
 				description:commandsList,
 				color:0x2471A3
-			}})
+			}});
 		}
 
 		if(input === prefix + 'PING'){
 			message.channel.send({embed:{
 				description:`Ping successful! The bot ${bot.user.tag}! is online!`,
 				color:0x2471A3
-			}})
+			}});
 		}
 
-		if(!userData[sender.id]){
-			userData[sender.id] = {
-				messagesSent: 0 };
-		}
-		userData[sender.id].messagesSent++;
-		fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
-			if(err){
-				console.error(err);
-			}
-		});
-
-		if(!profanities2[sender.id]){
-			profanities2[sender.id] = {
-				swearwords: 0 };
-		}
-
-		for(x = 0; x < profanities.length; x++){
-			swearword = profanities[x].toUpperCase();
-			if(input.includes(swearword)){
-
-				profanities2[sender.id].swearwords++;
-				fs.writeFile('Storage/profanities2.json', JSON.stringify(profanities2), (err) => {
-					if(err){
-						console.error(err);
-					}
-				});
-
-				message.delete();
-				sender.send({embed:{
-					description:'Hey! Word **' + swearword + '** is not allowed on our server. Please don\'t use it!',
+		if(input === prefix + 'PLAY'){
+			if(!args[1]){
+				message.channel.send({embed:{
+					description:'You need to provide a link!',
 					color:0x2471A3
-				}})
+				}});
 				return;
 			}
-		}
-
-		if(input.startsWith(prefix + 'USERINFO')){
-			var uicommand = input.toString().split(' ');
-			if(!uicommand[1]){
+			if(!(ytdl.validateURL(args[1]))){
 				message.channel.send({embed:{
-					description:userInfo(sender),
+					description:'You didn\'t provide a valid youtube link!',
 					color:0x2471A3
-				}})
-			}
-			// !!!***JAKO BITNO!!!*** Složiti za ostale usere
-		}
-
-		if(input.startsWith(prefix + 'CLR')){
-			var clrcommand = input.toString().split(' ');
-			if(!clrcommand[1]){
-				message.channel.send({embed:{
-					description:'Error! Please specify the number of messages to clear e.g. <clr 10',
-					color:0x2471A3
-				}})
+				}});
 				return;
-			}
-			if((clrcommand[1] < 1) || (clrcommand[1] > 99)){
-				message.channel.send({embed:{
-					description:'You need to enter a number between 1 and 99!',
-					color:0x2471A3
-				}})
 			}
 			else{
-				if(sender.id === '764170607004745739'){
-					nmb=Number(clrcommand[1])+1;
-					message.channel.bulkDelete(nmb);
-				}
-				else{
+				if(!message.member.voice.channel){
 					message.channel.send({embed:{
-						description:'You do not have permissions to request the deletion of messages on this server!',
+						description:'You must be in a voice channel to play the music!',
 						color:0x2471A3
-					}})
+					}});
 					return;
 				}
+				if(!servers[message.guild.id]){
+					servers[message.guild.id] = {
+						queue: []
+					};
+				}
+
+				server = servers[message.guild.id];
+				server.queue.push(args[1]);
+
+				function play(connection, message){
+					server = servers[message.guild.id];
+					server.dispatcher = connection.play(ytdl(server.queue[0], {filter: 'audioonly'}));
+					server.queue.shift();
+
+					server.dispatcher.on('finish', () => {
+						if(server.queue[0]){
+							play(connection, message);
+							message.channel.send('IF!');
+						} else {
+							connection.disconnect();
+							joinstatus='waiting';
+							servers[message.guild.id] = '';
+							message.channel.send('ELSE!');
+						}
+					});
+				}
+
+				if(joinstatus==='waiting'){
+					message.member.voice.channel.join().then(function(connection){
+						joinstatus='joined';
+						play(connection, message);
+						message.channel.send('JOINED!');
+					});
+				}
+			}
+		}
+
+		if(input === prefix + 'SKIP'){
+			if(joinstatus==='joined'){
+				server = servers[message.guild.id];
+				if(server.dispatcher){
+					server.dispatcher.end();
+					message.channel.send('SKIP!');
+				}
+			}
+			else{
+				return;
+			}
+		}
+
+		if(input === prefix + 'STOP'){
+			if(joinstatus==='joined'){
+				server = servers[message.guild.id];
+				server.dispatcher.end();
+				message.channel.send('STOP!');
+			}
+			else{
+				return;
 			}
 		}
 	});
-
+	
 	bot.on('ready', function(ready){
 
 		console.log(`Logged in as ${bot.user.tag}!`);
 		bot.user.setStatus('Online');
-	});
-
-	bot.on('guildMemberAdd', function(member){
-
-		var channel = bot.channels.cache.get('778340125389488169');
-		channel.send({embed:{
-			description:`Hey ${member}, welcome to **ElJefe Discord Server**! Make sure to read and follow all rules.`,
-			color:0x2471A3
-		}})
-	});
-
-	bot.on('guildMemberRemove', function(member){
-
-		var channel = bot.channels.cache.get('783416981776498748');
-		channel.send({embed:{
-			description:`User ${member.user.username} left the **ElJefe Discord Server**. We're sorry to see you go :disappointed_relieved:.`,
-			color:0x2471A3
-		}})
 	});
 
 	bot.login(process.env.DISCORD_TOKEN);
