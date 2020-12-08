5da9e2559d5e87a954a3f30831e42274a4ad9358
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-08 14:12:37 +0100

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+module.exports.run = async (bot, message, args) => {
+	//if(input === prefix + 'PING'){
+		message.channel.send({embed:{
+			description:`Ping successful! The bot ${bot.user.tag}! is online!`,
+			color:0x2471A3
+		}});
+	//}
+}
+module.exports.config = {
+	commands: 'ping'
+}
