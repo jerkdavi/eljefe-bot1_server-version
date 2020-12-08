62c96eb80d6c4b57c76793be5abe8a47f3b90b55
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-08 19:05:09 +0100

    Update USERINFO.js

--- commands/USERINFO.js
+++ commands/USERINFO.js
@@ -1,15 +1,10 @@
 	/*jshint esversion: 6 */
 	
-	var input = message.content.toUpperCase();
-	
 	module.exports.run = async (bot, message, args) => {
-		var uicommand = input.toString().split(' ');
-		if(!uicommand[1]){
-			message.channel.send({embed:{
-				description:userInfo(sender),
-				color:0x2471A3
-			}});
-		}
+		message.channel.send({embed:{
+			description:userInfo(sender),
+			color:0x2471A3
+		}});
 		// !!!***JAKO BITNO!!!*** Složiti za ostale usere
 	}
 	module.exports.config = {
