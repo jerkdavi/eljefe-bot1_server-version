bb5f0a9edebe170d2fdaefabd0e647d5ea0fe45f
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-08 18:40:05 +0100

    Update PING.js

--- commands/PING.js
+++ commands/PING.js
@@ -2,10 +2,10 @@
 
 	module.exports.run = async (bot, message, args) => {
 		message.channel.send({embed:{
-			description:`Ping successful! The bot ${bot.user.tag}! is online!`,
+			description:`Ping successful1! The bot ${bot.user.tag}! is online!`,
 			color:0x2471A3
 		}});
 	}
 	module.exports.config = {
-		commands:'PING'
+		command:'PING'
 	}
