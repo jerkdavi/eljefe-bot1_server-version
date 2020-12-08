88722b7fa88e50d477c48280365926e70f27bce0
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-08 19:15:17 +0100

    Update USERINFO.js

--- commands/USERINFO.js
+++ commands/USERINFO.js
@@ -2,20 +2,21 @@
 
 	var fs = require('fs');
 	var userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
-	var sender = message.author;
-
-	function userInfo(user){
-		var finalString = '';
-		finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
-
-		var userCreated = user.createdAt.toString().split(' ');
-		finalString += 'Date created **' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**\n';
-
-		finalString += 'Message sent to this server: **' + userData[user.id].messagesSent + '** messages';
-		return finalString;
-	}
 
 	module.exports.run = async (bot, message, args) => {
+		var sender = message.author;
+
+		function userInfo(user){
+			var finalString = '';
+			finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
+
+			var userCreated = user.createdAt.toString().split(' ');
+			finalString += 'Date created **' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**\n';
+
+			finalString += 'Message sent to this server: **' + userData[user.id].messagesSent + '** messages';
+			return finalString;
+		}
+
 		message.channel.send({embed:{
 			description:userInfo(sender),
 			color:0x2471A3
