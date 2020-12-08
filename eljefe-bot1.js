e692d4e53a15b3f666756ec2e7d53d9062f2dd8e
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-08 14:50:42 +0100

    Update eljefe-bot1.js

--- eljefe-bot1.js
+++ eljefe-bot1.js
@@ -48,6 +48,7 @@
 		var input = message.content.toUpperCase();
 		var cont = input.slice(prefix.length).split(' ');
 		console.log('input: '+input);
+		console.log('prefix: '+prefix);
 		console.log('cont: '+cont);
 		var args = cont.slice(1);
 		console.log('args: '+args);
