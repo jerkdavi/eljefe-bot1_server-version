4396710ad3ec6fca2ac59c2f5417770976e34758
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-09 20:29:28 +0100

    Update USERINFO.js

--- commands/USERINFO.js
+++ commands/USERINFO.js
@@ -1,24 +1,14 @@
 	/*jshint esversion: 6 */
 
+	var fs = require('fs');
+
 	module.exports.run = async (bot, message, args, userData) => {
-		var fs = require('fs');
-		//var userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
+		
 		var sender = message.author;
 		var input = message.content.toUpperCase();
 		var uicommand = input.toString().split(' ');
 
-		/*function userInfo(user, guild){
-			var finalString = '';
-			finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
-
-			var userCreated = user.createdAt.toString().split(' ');
-			finalString += 'Date created **' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**\n';
-
-			finalString += 'Message sent to this server: **' + userData[user.id + guild.id].messagesSent + '** messages';
-			return finalString;
-		}*/
-
-		function userInfo(user){
+		/*function userInfo(user){
 			var finalString = '';
 			finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
 
@@ -27,21 +17,33 @@
 
 			finalString += 'Message sent to this server: **' + userData[user.id].messagesSent + '** messages';
 			return finalString;
+		}*/
+
+		function userInfo(user, guild){
+			var finalString = '';
+			finalString += '**User Info:**\nUser name: **' + user.username + '**\nUser ID: **' + user.id + '**\n';
+
+			var userCreated = user.createdAt.toString().split(' ');
+			finalString += 'Date created **' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**\n';
+
+			finalString += 'Message sent to this server: **' + userData[user.id + guild.id].messagesSent + '** messages';
+			return finalString;
 		}
 
 		/*if(!uicommand[1]){
 			message.channel.send({embed:{
-				description:userInfo(sender, message.guild.id),
+				description:userInfo(sender),
 				color:0x2471A3
 			}});
 		}*/
 
 		if(!uicommand[1]){
 			message.channel.send({embed:{
-				description:userInfo(sender),
+				description:userInfo(sender, message.guild.id),
 				color:0x2471A3
 			}});
 		}
+
 		// !!!***JAKO BITNO!!!*** Složiti za ostale usere
 	}
 	module.exports.config = {
