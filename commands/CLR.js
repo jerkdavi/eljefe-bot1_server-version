f6ad7152f85d173364eb9d9a02bea86a70758787
jerkdavi <75536158+jerkdavi@users.noreply.github.com>
2020-12-14 13:34:25 +0100

    Update CLR.js

--- commands/CLR.js
+++ commands/CLR.js
@@ -4,46 +4,54 @@
 		//console.log('Step 101');
 		let sender = message.author;
 		//console.log('Step 102');
-		let input = message.content.toUpperCase();
+		let prefix = process.env.prefix;
 		//console.log('Step 103');
-		let clrcommand = input.toString().split(' ');
-		//console.log('Step 104');
-		if(!clrcommand[1]){
-			//console.log('Step 105');
-			message.channel.send({embed:{
-				description:'Error! Please specify the number of messages to clear e.g. <clr 10',
-				color:0x2471A3
-			}});
-			//console.log('Step 106');
-			return;
-		}
-		//console.log('Step 107');
-		if((clrcommand[1] < 1) || (clrcommand[1] > 99)){
-			//console.log('Step 108');
-			message.channel.send({embed:{
-				description:'You need to enter a number between 1 and 99!',
-				color:0x2471A3
-			}});
-			//console.log('Step 109');
-			return;
-		}
-		//console.log('Step 110');
 		if(!(sender.id === '764170607004745739')){
-			//console.log('Step 111');
+			//console.log('Step 104');
 			message.channel.send({embed:{
 				description:'You do not have permissions to request the deletion of messages on this server!',
 				color:0x2471A3
 			}});
-			//console.log('Step 112');
+			//console.log('Step 105');
 			return;
 		}
-		//console.log('Step 113');
-		let nmb=Number(clrcommand[1])+1;
-		//console.log('Step 114');
-		message.channel.bulkDelete(nmb);
+		//console.log('Step 106');
+		if(!args[1]){
+			//console.log('Step 107');
+			message.channel.send({embed:{
+				description:`The amount of messages must be specified!\nUsage: **${prefix}CLR <amount>**.`,
+				color:0x2471A3
+			}});
+			//console.log('Step 108');
+			return;
+		}
+		//console.log('Step 109');
+		if(isNaN(args[1])){
+			//console.log('Step 110');
+			message.channel.send({embed:{
+				description:`The amount has to be a number!\nUsage: **${prefix}CLR <amount>**.`,
+				color:0xD4AF37
+			}});
+			//console.log('Step 111');
+			return;
+		}
+		//console.log('Step 112');
+		if((args[1] < 1) || (args[1] > 99)){
+			//console.log('Step 113');
+			message.channel.send({embed:{
+				description:'You need to enter a number between 1 and 99!',
+				color:0x2471A3
+			}});
+			//console.log('Step 114');
+			return;
+		}
 		//console.log('Step 115');
+		let nmb=Number(args[1])+1;
+		//console.log('Step 116');
+		message.channel.bulkDelete(nmb);
+		//console.log('Step 117');
 	}
-	//console.log('Step 116');
+	//console.log('Step 118');
 	module.exports.config = {
 		command:'CLR'
 	}
